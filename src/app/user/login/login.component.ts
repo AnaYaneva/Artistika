0216c1be61b31ab8b1679a3d3b171b4ec2442490
Ana Yaneva <a.yaneva@seeburger.com>
2024-12-09 15:26:09 +0200

    login works

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -5,29 +5,39 @@ import { FormsModule, NgForm } from '@angular/forms';
 import { EmailDirective } from '../../directives/email.directive';
 import { DOMAINS } from '../../constants';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink, FormsModule, EmailDirective],
+  imports: [FormsModule,
+    RouterLink,
+    CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  domains = DOMAINS;
+  username = '';
+  password = '';
+  isLoading = false;
+  loginError = false;
 
-  constructor(private userService: UserService, private router: Router) { }
+  constructor(private authService: UserService, private router: Router) { }
 
-  login(form: NgForm) {
-    if (form.invalid) {
-      console.error('Invalid Login Form!');
-      return;
+  onSubmit(): void {
+    if (this.username && this.password) {
+      this.isLoading = true;
+      this.authService.login(this.username, this.password).subscribe({
+        next: (response) => {
+          this.isLoading = false; // Stop loading on success
+          this.router.navigate(['/home']); // Redirect to home on successful login
+        },
+        error: (err) => {
+          this.isLoading = false; // Stop loading on error
+          this.loginError = true; // Show error message
+          console.error('Login failed', err);
+        },
+      });
     }
-
-    const { email, password } = form.value;
-    // this.fireauth.signInWithEmailAndPassword(email, password);
-    this.userService.login(email, password).subscribe(() => {
-      this.router.navigate(['/']);
-    });
   }
 }
